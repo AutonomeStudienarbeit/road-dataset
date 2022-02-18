479b393950257e806b245ee2a0c703032ba02082
lkno0705 <lkno0705@gmail.com>
2022-02-18 14:23:23 +0100

    removed automatic unzip

--- road/get_dataset.sh
+++ road/get_dataset.sh
@@ -3,10 +3,10 @@
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1YQ9ap3o9pqbD0Pyei68rlaMDcRpUn-qz' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1YQ9ap3o9pqbD0Pyei68rlaMDcRpUn-qz" -O videos.zip
 
 #Unzipping the videos
-unzip videos.zip
+#unzip videos.zip
 
 #Removing the zip file
-rm videos.zip
+#rm videos.zip
 
 #Downloading annotation file for training and validation in json format
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1HAJpdS76TVK56Qvq1jXr-5hfFCXKHRZo' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1HAJpdS76TVK56Qvq1jXr-5hfFCXKHRZo" -O road_trainval_v1.0.json 
